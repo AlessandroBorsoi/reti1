aa9364b8fd6883191a46e72d4a3d569909fcc8e0
Alessandro Borsoi <alessandro.borsoi1984@gmail.com>
2020-05-13 11:59:43 +0200

    Add test on big file

--- test/test_splitter.c
+++ test/test_splitter.c
@@ -6,8 +6,11 @@
 
 char resource[512];
 static void test_create_destroy(const char *arg0);
-static void test_next(const char *arg0);
-static void get_resource(char *res_path, const char *argv0, char *buffer);
+static void test_next_with_example_txt(const char *arg0);
+static void test_next_with_big_example_txt(const char *arg0);
+static void get_resource(char *resource, const char *argv0, char *buffer);
+static void populate_file(char *resource);
+static void clean_file(char *resource);
 
 int main(int argc, char const *argv[])
 {
@@ -18,7 +21,8 @@ int main(int argc, char const *argv[])
 
     printf("Test case 'next'... ");
     fflush(stdout);
-    test_next(argv[0]);
+    test_next_with_example_txt(argv[0]);
+    test_next_with_big_example_txt(argv[0]);
     printf("OK\n");
 
     return 0;
@@ -51,7 +55,7 @@ void test_create_destroy(const char *arg0)
     upo_protocol_splitter_destroy(&splitter);
 }
 
-void test_next(const char *arg0)
+void test_next_with_example_txt(const char *arg0)
 {
     get_resource("../data/example.txt", arg0, resource);
     upo_protocol_splitter_t splitter = upo_protocol_splitter_create(resource);
@@ -93,7 +97,29 @@ void test_next(const char *arg0)
     upo_protocol_splitter_destroy(&splitter);
 }
 
-void get_resource(char *res_path, const char *argv0, char *buffer)
+void test_next_with_big_example_txt(const char *arg0)
+{
+    get_resource("../data/big_example.txt", arg0, resource);
+
+    populate_file(resource);
+
+    upo_protocol_splitter_t splitter = upo_protocol_splitter_create(resource);
+
+    assert(splitter != NULL);
+
+    assert(upo_protocol_splitter_is_valid(splitter));
+
+    char output[512] = {0};
+
+    while (strcmp(output, "0\n") != 0)
+        upo_protocol_splitter_next(splitter, output, 512);
+
+    upo_protocol_splitter_destroy(&splitter);
+
+    clean_file(resource);
+}
+
+void get_resource(char *resource, const char *argv0, char *buffer)
 {
     memset(buffer, 0, 512);
     getcwd(buffer, 512);
@@ -113,5 +139,20 @@ void get_resource(char *res_path, const char *argv0, char *buffer)
         token = strtok(NULL, "/");
     }
 
-    strcat(buffer, res_path);
+    strcat(buffer, resource);
+}
+
+void populate_file(char *resource)
+{
+    FILE *fptr = fopen(resource, "w");
+    for (int i = 1; i <= 1000000; i++)
+        fprintf(fptr, "%d ", i);
+    fclose(fptr);
+}
+
+void clean_file(char *resource)
+{
+    FILE *fptr = fopen(resource, "w");
+    fprintf(fptr, "");
+    fclose(fptr);
 }
